647010d88eccd9fc236e1031fbac51b081b54ef9
kemalgencay <kemalgencay@gmail.com>
2026-01-19 20:17:03 +0300

    1

--- routes/index.tsx
+++ routes/index.tsx
@@ -18,7 +18,7 @@ export default define.page(function Home(ctx) {
           height="128"
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <h1 class="text-4xl font-bold">Fresh'e Hoş Geldiniz</h1>
         <p class="my-4">
           Try updating this message in the
           <code class="mx-2">./routes/index.tsx</code> file, and refresh.
