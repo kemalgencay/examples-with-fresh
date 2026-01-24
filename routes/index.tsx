bc8dc3350a61da1f0886a22b36f7fb7144e3ca9a
kemalgencay <kemalgencay@gmail.com>
2026-01-24 12:46:43 +0300

    8

--- routes/index.tsx
+++ routes/index.tsx
@@ -18,7 +18,7 @@ export default define.page(function Home(ctx) {
           height="128"
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
-        <h1 class="text-4xl font-bold">Fresh Example 3
+        <h1 class="text-4xl font-bold">Fresh Example 4
 
         </h1>
         <p class="my-4">
